dbd0cfe45fe775094f5b4907dee2c3c475fd7f44
Joe Stanton <joe.stanton@red-badger.com>
2014-02-26 13:31:06 +0000

    Setup basic app with express server for all routes

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.use(express.logger('dev'));
+
+// App Express Setup
+app.use(express.static(__dirname));
+
+app.get('*', function(req, res) {
+  return res.sendfile('index.html');
+});
+
+app.listen(process.env.PORT || 5000);
